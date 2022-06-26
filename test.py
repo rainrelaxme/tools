8613c68b14e5787ab34d1a0def547c11a745ee5d
rainrelaxme <zxr1110.love@163.com>
2022-06-27 00:57:53 +0800

    打开本地文件夹

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
- a=, b, c, d = 20, 5.5, True, 4+3j
-print(type(a), type(b), type(c), type(d))
-print (a)
+# import os,tkinter as tk
+# from tkinter import filedialog
+#
+# EAN_list = []
+# root = tk.Tk()
+# root.withdraw()
+# # Folderpath = filedialog.askdirectory()
+# file_path = filedialog.askopenfilenames()
+# # print("Folderpath:" + Folderpath)
+# for i in file_path:
+#     print("Filepath:" + i)
+# # print("Filepath:" + file_path)
+
+import cr
