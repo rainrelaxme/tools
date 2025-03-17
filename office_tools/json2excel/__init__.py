18585a1a199a9a5e1128ca92e0f3c45bb944939c
“Rainrelaxme” <rainrelaxme@foxmail.com>
2025-03-18 00:12:22 +0800

    整理结构

---
+++ office_tools/json2excel/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+@Project: distance.py
+@Version:
+@Author: RainRelaxMe
+@Date: 2025/3/17 23:38
+"""
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Python')
