8c9eb0e58e6619cb8748164120ade98aa930080a
shawn <xiaoran.zhu@cmtech.com>
2025-09-09 17:29:25 +0800

    feature: SOP_translate

--- app/file_content/SOP_translate/test.py
+++ app/file_content/SOP_translate/test.py
@@ -7,8 +7,18 @@ if __name__ == '__main__':
     output_file = f"../../../file/temp/outputxxx-{datetime}.docx"
     doc = Document()
 
-    para = doc.add_paragraph('test text content.')
-    para.runs.font.name = '宋体'
-    para.font.size = Pt(12)
+    para = doc.add_paragraph()
+
+    run = para.add_run('你好啊，鲍老师')
+    run2 = para.add_run('test text content.')
+
+    # run.font.name = 'Times New Roman'
+
+    run.font.name = u'宋体'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
+    run.font.size = Pt(36)
+
+    run2.font.name = '宋体'
+    run2.font.size = Pt(12)
 
     doc.save(output_file)
